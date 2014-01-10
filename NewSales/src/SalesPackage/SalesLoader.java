e555a6dc1b6b678a973ed28c7a1eb03b31876f68
Abhijit <abhijit.govardhan@lowes.com>
2014-01-09 22:01:55 -0500

    My first commit

---
+++ NewSales/src/SalesPackage/SalesLoader.java
@@ -0,0 +1,15 @@
+package SalesPackage;
+
+public class SalesLoader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("-------------*------------");
+		System.out.println("Hello Happy New Sales People in US !!!");
+		System.out.println ("This Application Shows New Sales Data");
+		System.out.println ("-------------*------------");
+		
+		
+	}
+
+}
